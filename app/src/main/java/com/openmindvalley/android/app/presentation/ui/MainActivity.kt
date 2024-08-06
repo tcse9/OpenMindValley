f3984687c41cc71c69b460844fb473f50ebb4adf
taufiq <ovioviovi@gmail.com>
2024-08-06 13:10:01 +0600

    added category chips as the bottom item of channel

--- app/src/main/java/com/openmindvalley/android/app/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/openmindvalley/android/app/presentation/ui/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.openmindvalley.android.app.presentation.ui
 
 import android.os.Bundle
-import android.view.WindowManager
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import com.openmindvalley.android.app.presentation.composables.RootView
 import com.openmindvalley.android.app.presentation.theme.OpenMindValleyTheme
@@ -17,20 +16,20 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     private val viewModel: MainViewModel by viewModels()
 
-    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
-            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.dark(
+                android.graphics.Color.TRANSPARENT,
+            )
         )
+
+        super.onCreate(savedInstanceState)
 
         viewModel.getMediaNewEpisode("z5AExTtw")
         viewModel.getMediaChannel("Xt12uVhM")
         viewModel.getMediaCategories("A0CgArX3")
 
-        enableEdgeToEdge()
+
         setContent {
             OpenMindValleyTheme {
                 Scaffold { innerPadding ->
