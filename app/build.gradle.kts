e963ac811566eac1e60ca480bf8a5a0e6c48f24f
taufiq <ovioviovi@gmail.com>
2024-09-17 00:57:43 +0600

    added new version and branch

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 21
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0.2"
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
